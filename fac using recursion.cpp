85664195f84e58c476a416425512047f4c2bf7c2
HIMASAGARMVSN <123452290+HIMASAGARMVSN@users.noreply.github.com>
2023-08-28 12:47:29 +0530

    Add files via upload

---
+++ fac using recursion.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void fact (int n) {
+	int i;
+	for(i=1;i<=n;i++){
+		//fact = fact*i;
+		}
+}
+int main(){
+	int n;
+	printf("enter the range:");
+	scanf("%d",&n);
+	fact (n);
+	printf("%d",fact);
+	return 0;
+}
